80c410073d9925294979671980a54b61bb3dc964
kuhaniz <tanjinwei@gmail.com>
2016-11-21 23:59:15 +0800

    final email fix

--- dist/email.php
+++ dist/email.php
@@ -1,5 +1,14 @@
 <?php
 
+$response = array('Status' => TRUE, 'Message' => NULL);
+
+//antispam
+if($_POST['url'] != ""){
+
+    $response = array('Status' => False, 'Message' => "Spam");
+}else{
+
+
   $category = $_POST['category'] ;
   $email = $_POST['email'] ;
   $name = $_POST['name'] ;
@@ -16,29 +25,34 @@
 
 
 <?php
-require '/mail/PHPMailerAutoload.php';
+  require 'mail/PHPMailerAutoload.php';
 
-$mail = new PHPMailer;
+  $mail = new PHPMailer();
 
-$mail->SMTPDebug = 2;
+  //$mail->SMTPDebug = 2;
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->SMTPAuth = true; // authentication enabled
-$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-$mail->Host = "smtp.gmail.com";
-$mail->Port = 465; // or 587
-$mail->Username = "apptoku@gmail.com";
-$mail->Password = "Qwerty88!";
+  $mail->isSMTP();                                      // Set mailer to use SMTP
+  $mail->SMTPAuth = true; // authentication enabled
+  $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+  $mail->Host = "smtp.gmail.com";
+  $mail->Port = 465; // or 587
+  $mail->Username = "apptoku@gmail.com";
+  $mail->Password = "Qwerty88!";
 
-$mail->setFrom('apptoku@gmail.com', 'Toku');
-$mail->addAddress('tanjinwei@gmail.com', 'Toku');     // Add a recipient
+  $mail->setFrom('apptoku@gmail.com', 'Toku');
+  $mail->addAddress('tanjinwei@gmail.com', 'Jin Wei');
+  $mail->addAddress('support@tokuapp.com', 'Toku');       // Add a recipient
 
-$mail->Subject = 'Feedback Form - '.$subject;
-$mail->Body    =  $body;
+  $mail->Subject = 'Contact Us Form - '.$subject;
+  $mail->Body    =  $body;
 
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    header('Location: thank-you.html');
-}
+
+
+  if(!$mail->send()) {
+      $response['Status'] = FALSE;
+      $response['Message'] = $mail->ErrorInfo;
+  }
+
+} //end check spam
+
+echo json_encode($response);
