744aaa1ef518cdb2d1ed10132b13f1052e92df43
jtan6 <jtan6@sapient.com>
2016-12-07 23:23:34 +0800

    faq page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -68,10 +68,19 @@ $(document).ready(function(e){
 }
 
 
+  $('a.accordion').on('click', function(e) {
+    e.preventDefault();
+    //console.log($(this).next('.panel'));
+    $(this).toggleClass('active');
+    $(this).next('.panel').slideToggle(300);
+  });
+
+
+
 })
 
 /* Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. The "active" class is used to add a background color to the current button when its belonging panel is open. The "show" class is used to open the specific accordion panel */
-var acc = document.getElementsByClassName('accordion');
+/*var acc = document.getElementsByClassName('accordion');
 var i;
 
 for (i = 0; i < acc.length; i++) {
@@ -79,4 +88,4 @@ for (i = 0; i < acc.length; i++) {
         this.classList.toggle('active');
         this.nextElementSibling.classList.toggle('show');
     }
-}
+}*/
